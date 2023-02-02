be9cbcf08496dd5011243dd08e1e7768a7e9f9b5
Niels <n.koster@portavita.eu>
2023-02-02 14:47:31 +0100

    Add GET endpoint for bar code query

--- server.js
+++ server.js
@@ -9,6 +9,27 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors());
 
+app.get('/product/:code', (req, res) => {
+  const code = req.params.code
+  const fileName = `${process.env.PRODUCTS_DIR}/${code}.json`
+  console.log(code, fileName)
+  if (!code) {
+    res.status(400).send(JSON.stringify({
+      message: `Product code is required`
+    }))
+  }
+  res.sendFile(fileName, {root: __dirname}, (error) => {
+    if (error) {
+      console.error(error)
+      res.status(404).send(JSON.stringify({
+        message: `Product ${code} not found`
+      }))
+    } else {
+      console.log(`Sent ${fileName}`)
+    }
+  })
+})
+
 app.post('/product', (req, res) => {
   const product = req.body
   const code = product.code
